a65139abfb5880b314721e599ca577d0faeb9082
Cesar Murillo <namiayumis@gmail.com>
2020-02-07 11:48:09 +0900

    carga condicional

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@ function App() {
   // definir el state
   const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
+  const [mostrarpregunta, actualizarPregunta] = useState(true);
 
   return (
     <div className="container">
       <header>
         <h1>Gasto Semanal</h1>
         <div className="contenido-principal contenido">
-          <Pregunta
-            guardarPresupuesto={guardarPresupuesto}
-            guardarRestante={guardarRestante}
-          />
-          <div className="row">
-            <div className="one-half column">
-              <Formulario />
-            </div>
+          {mostrarpregunta ? (
+            <Pregunta
+              guardarPresupuesto={guardarPresupuesto}
+              guardarRestante={guardarRestante}
+              actualizarPregunta={actualizarPregunta}
+            />
+          ) : (
+            <div className="row">
+              <div className="one-half column">
+                <Formulario />
+              </div>
 
-            <div className="one-half column">2</div>
-          </div>
+              <div className="one-half column">2</div>
+            </div>
+          )}
         </div>
       </header>
     </div>
--- src/Components/Pregunta.js
+++ src/Components/Pregunta.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import Error from "./Error";
 
-const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
+const Pregunta = ({
+  guardarPresupuesto,
+  guardarRestante,
+  actualizarPregunta
+}) => {
   // definir el state
   const [cantidad, guardarCantidad] = useState(0);
 
@@ -26,6 +30,7 @@ const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
     guardarError(false);
     guardarPresupuesto(cantidad);
     guardarRestante(cantidad);
+    actualizarPregunta(false);
   };
 
   return (
